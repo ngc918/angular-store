33aa7d21a2a34af787775a1046fa767cde995af1
ngc.918 <nicolas.g9182@gmail.com>
2022-11-28 23:48:27 -0500

    ~

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CartComponent } from './cart/cart.component';
+import { CartComponent } from './components/cart/cart.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CartComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent, CartComponent, HeaderComponent],
+  imports: [BrowserModule, MatToolbarModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
